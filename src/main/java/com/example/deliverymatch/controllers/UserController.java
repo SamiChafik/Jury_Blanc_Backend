c3599255a0b171aba4d618b9348b09572d41e25c
SamiChafik <sami.chafik00@gmail.com>
2025-06-18 14:36:33 +0100

    renamed file

---
+++ src/main/java/com/example/deliverymatch/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.example.deliverymatch.controllers;
+
+import com.example.deliverymatch.Services.UserService;
+import com.example.deliverymatch.dtos.UserDTO;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/userList")
+    public List<UserDTO> getUsers(){
+        return service.getUsersList();
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'DRIVER', 'SENDER')")
+    @PutMapping("/update/{id}")
+    public UserDTO updateUser(@RequestBody UserDTO dto, @PathVariable Long id){
+        return service.updateUser(id,dto);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteUser(@PathVariable Long id){
+        service.deleteUser(id);
+    }
+
+}
