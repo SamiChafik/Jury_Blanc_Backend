0534af2d9ee253b584d5175719fe00853dec0a59
SamiChafik <sami.chafik00@gmail.com>
2025-06-23 00:57:42 +0100

    CrossOrigin

--- src/main/java/com/example/deliverymatch/controllers/AnnouncementController.java
+++ src/main/java/com/example/deliverymatch/controllers/AnnouncementController.java
@@ -6,12 +6,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/announcements")
+@CrossOrigin("*")
 public class AnnouncementController {
 
     private final AnnouncementService announcementService;
@@ -20,32 +22,46 @@ public class AnnouncementController {
         this.announcementService = announcementService;
     }
 
+    @PreAuthorize("hasRole('DRIVER')")
     @PostMapping
     public ResponseEntity<AnnouncementDTO> createAnnouncement(@RequestBody AnnouncementDTO announcementDTO) {
         AnnouncementDTO createdAnnouncement = announcementService.createAnnouncement(announcementDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdAnnouncement);
     }
 
+    @PreAuthorize("hasAnyRole('DRIVER', 'ADMIN', 'SENDER')")
     @GetMapping
     public ResponseEntity<List<AnnouncementDTO>> getAllAnnouncements() {
         List<AnnouncementDTO> announcements = announcementService.getAllAnnouncements();
         return ResponseEntity.ok(announcements);
     }
 
+    @PreAuthorize("hasAnyRole('DRIVER', 'ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<AnnouncementDTO> getAnnouncementById(@PathVariable Long id) {
         AnnouncementDTO announcement = announcementService.getAnnouncementById(id);
         return ResponseEntity.ok(announcement);
     }
 
+    @PreAuthorize("hasAnyRole('DRIVER', 'ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<AnnouncementDTO> updateAnnouncement(
             @PathVariable Long id,
-            @Valid @RequestBody AnnouncementDTO announcementDTO) {
+            @RequestBody AnnouncementDTO announcementDTO) {
         AnnouncementDTO updatedAnnouncement = announcementService.updateAnnouncement(id, announcementDTO);
         return ResponseEntity.ok(updatedAnnouncement);
     }
 
+    @PreAuthorize("hasRole('DRIVER')")
+    @PutMapping("/updateStatus/{id}")
+    public ResponseEntity<AnnouncementDTO> updateStatus(
+            @PathVariable Long id,
+            @RequestBody AnnouncementDTO announcementDTO) {
+        AnnouncementDTO updatedAnnouncement = announcementService.updateStatus(id, announcementDTO);
+        return ResponseEntity.ok(updatedAnnouncement);
+    }
+
+    @PreAuthorize("hasAnyRole('DRIVER', 'ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAnnouncement(@PathVariable Long id) {
         announcementService.deleteAnnouncement(id);
