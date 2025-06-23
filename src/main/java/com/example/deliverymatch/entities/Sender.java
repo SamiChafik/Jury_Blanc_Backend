76473a6c2ab38e39085718f9dde184f84b251c36
SamiChafik <sami.chafik00@gmail.com>
2025-06-23 01:00:10 +0100

    relation request|sender

--- src/main/java/com/example/deliverymatch/entities/Sender.java
+++ src/main/java/com/example/deliverymatch/entities/Sender.java
@@ -1,7 +1,27 @@
 package com.example.deliverymatch.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Sender extends User {
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Request> request = new ArrayList<>();
+
+    public List<Request> getRequest() {
+        return request;
+    }
+
+    public void setRequest(List<Request> request) {
+        this.request = request;
+    }
 }
