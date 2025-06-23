1e315ff16aeda5318ff5c6b0afe2b763ca7e87c6
SamiChafik <sami.chafik00@gmail.com>
2025-06-23 01:00:40 +0100

    added custom funcs

--- src/main/java/com/example/deliverymatch/repositories/RequestRepository.java
+++ src/main/java/com/example/deliverymatch/repositories/RequestRepository.java
@@ -1,4 +1,12 @@
 package com.example.deliverymatch.repositories;
 
-public interface RequestRepository {
+import com.example.deliverymatch.entities.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RequestRepository extends JpaRepository<Request, Long> {
+    List<Request> findAllBySenderEmail(String senderEmail);
+
+    List<Request> findAllByAnnouncementId(Long id);
 }
